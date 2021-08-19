e3422c1737cdb0bf60dc9fde504bee1b52a450ff
Subadra bro <87601796+SubadraBro@users.noreply.github.com>
2021-08-18 19:02:10 -0700

    Create own.js

---
+++ plugins/own.js
@@ -0,0 +1,23 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://i.ibb.co/Vv0fwvX/2017-08-24-31477-1503558583-large.png";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `👆👆👆☝☝☝\nMyowner Logos`}) 
+
+}));
