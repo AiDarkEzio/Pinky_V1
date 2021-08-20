a1d8d093b05dcfa89b8a9c4efc815fa86b2d1353
Subadra bro <87601796+SubadraBro@users.noreply.github.com>
2021-08-19 23:24:05 -0700

    Update img bts exo black pinnk.js

--- plugins/img bts exo black pinnk.js
+++ plugins/img bts exo black pinnk.js
@@ -10,7 +10,7 @@ const Lang = Language.getString('random_pic');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'bts ?(.*)', fromMe: true,  deleteCommand: false,  desc: Lang.BTS_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'bts ?(.*)', fromMe: true,  deleteCommand: false}, (async (message, match) => {
 
     var webimage = await axios.get(`https://api.lolhuman.xyz/api/random/bts?apikey=queenamdipublic`, { responseType: 'arraybuffer' })
 
@@ -18,7 +18,7 @@ if (Config.WORKTYPE == 'private') {
 
     }));
 
-    Asena.addCommand({pattern: 'exo ?(.*)', fromMe: true,  deleteCommand: false,  desc: Lang.EXO_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'exo ?(.*)', fromMe: true,  deleteCommand: false}, (async (message, match) => {
 
     var webimage = await axios.get(`https://api.lolhuman.xyz/api/random/exo?apikey=queenamdipublic`, { responseType: 'arraybuffer' })
 
@@ -26,7 +26,7 @@ if (Config.WORKTYPE == 'private') {
 
     }));
 
-    Asena.addCommand({pattern: 'blackpink ?(.*)', fromMe: true,  deleteCommand: false,  desc: Lang.BP_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'blackpink ?(.*)', fromMe: true,  deleteCommand: false}, (async (message, match) => {
 
     var webimage = await axios.get(`https://api.lolhuman.xyz/api/random/blackpink?apikey=queenamdipublic`, { responseType: 'arraybuffer' })
 
@@ -36,7 +36,7 @@ if (Config.WORKTYPE == 'private') {
 }
 
 else if (Config.WORKTYPE == 'public') {
-    Asena.addCommand({pattern: 'bts ?(.*)', fromMe: false, desc: Lang.BTS_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'bts ?(.*)', fromMe: false}, (async (message, match) => {
 
     var webimage = await axios.get(`https://api.lolhuman.xyz/api/random/bts?apikey=queenamdipublic`, { responseType: 'arraybuffer' })
 
@@ -44,7 +44,7 @@ else if (Config.WORKTYPE == 'public') {
 
     }));
 
-   Asena.addCommand({pattern: 'exo ?(.*)', fromMe: false, desc: Lang.EXO_DESC}, (async (message, match) => {
+   Asena.addCommand({pattern: 'exo ?(.*)', fromMe: false}, (async (message, match) => {
 
     var webimage = await axios.get(`https://api.lolhuman.xyz/api/random/exo?apikey=queenamdipublic`, { responseType: 'arraybuffer' })
 
@@ -52,7 +52,7 @@ else if (Config.WORKTYPE == 'public') {
 
     }));
 
-    Asena.addCommand({pattern: 'blackpink ?(.*)', fromMe: false, desc: Lang.BP_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'blackpink ?(.*)', fromMe: false}, (async (message, match) => {
 
     var webimage = await axios.get(`https://api.lolhuman.xyz/api/random/blackpink?apikey=queenamdipublic`, { responseType: 'arraybuffer' })
 
