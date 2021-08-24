53ef66174961134b41885e27cd301877504f6b0a
Subadra bro <scienceposhitha@gmail.com>
2021-08-23 21:25:36 -0700

    first commi

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *Pinky Bot*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *Pinky Bot*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *Pinky Bot*:  ```" + msg + "```"
 }
 
 
